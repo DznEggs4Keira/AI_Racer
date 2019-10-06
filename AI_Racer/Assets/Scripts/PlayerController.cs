c79bfb1c8874a1f29ddf299f600370f89e32047c
DznEggs4Keira <DznEggs4Keira@users.noreply.github.com>
2019-10-06 15:38:17 +0400

    Main Scene
    Main Scene is Setup... working on the AI scripts now......

---
+++ AI_Racer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float Horizontal; //what side is the player wanting to move the line to in the horizontal axis
+    float amount = 3.5f; //by what speed it should move
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //store the direction in the horiozntal axis
+        Horizontal = Input.GetAxis("Horizontal");
+
+        //Create a vector which will store which direction to move in
+        Vector2 move = new Vector2(Horizontal, 0);
+
+        //create a vector which will store the current transform's pos
+        Vector2 position = transform.position;
+
+        //increment it with the direction * the amount(speed) and then delta time to have movement be per second rather than per frame
+        position += move * amount * Time.deltaTime;
+
+        //set the new incremented position as the current value of the transform
+       transform.position = position;
+    }
+}
---
+++ AI_Racer/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    Material ScrollingMaterial;
+    Vector2 offset;
+
+    int xValue = 0;
+    int yValue = 5;
+
+    private void Awake()
+    {
+        ScrollingMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xValue, yValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScrollingMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
